272a7d6b06c1de296c8f60336dbe2f72b2ef07e5
Likhon Sheikh <likhondocs@gmail.com>
2024-10-02 00:41:18 +0600

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,12 +1,20 @@
 const Fauxid = require('../src/fauxid');
 
 module.exports = async (req, res) => {
-    try {
-        const fauxid = new Fauxid();
-        const data = await fauxid.result();
-        res.status(200).json(data);
-    } catch (error) {
-        console.error('Error generating random info:', error);
-        res.status(500).json({ error: 'An error occurred while generating random information' });
-    }
+  try {
+    // Create an instance of the Fauxid class
+    const fauxid = new Fauxid();
+
+    // Generate random information using the Fauxid instance
+    const data = await fauxid.result();
+
+    // Send the generated data as a JSON response with a 200 status code
+    res.status(200).json(data);
+  } catch (error) {
+    // Log the error to the console
+    console.error('Error generating random info:', error);
+
+    // Send an error response with a 500 status code and an error message
+    res.status(500).json({ error: 'An error occurred while generating random information' });
+  }
 };
